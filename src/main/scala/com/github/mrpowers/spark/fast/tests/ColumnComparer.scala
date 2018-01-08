8c7794d73c670c96845c9e489ec20dd38f4d7870
Xavier Ramirez <xavier@dreamcode.io>
2018-01-08 17:21:33 -0500

    Column comparer array support

--- src/main/scala/com/github/mrpowers/spark/fast/tests/ColumnComparer.scala
+++ src/main/scala/com/github/mrpowers/spark/fast/tests/ColumnComparer.scala
@@ -2,6 +2,8 @@ package com.github.mrpowers.spark.fast.tests
 
 import org.apache.spark.sql.DataFrame
 
+import scala.collection.mutable
+
 case class ColumnMismatch(smth: String) extends Exception(smth)
 
 trait ColumnComparer {
@@ -14,16 +16,31 @@ trait ColumnComparer {
     val elements = df.select(colName1, colName2).collect()
     val colName1Elements = elements.map(_(0))
     val colName2Elements = elements.map(_(1))
-    val mismatchMessage = s"""
+    val mismatchMessage =
+      s"""
 Columns aren't equal
 '$colName1' elements:
 [${colName1Elements.mkString(", ")}]
 '$colName2' elements:
 [${colName2Elements.mkString(", ")}]
 """
-    if (!colName1Elements.sameElements(colName2Elements)) {
+
+    val isNotArrayElement = colName1Elements.zip(colName2Elements).map((tuple) => {
+      tuple._1 match {
+        case a: mutable.WrappedArray[_] =>
+          if (!(a.toSet == tuple._2.asInstanceOf[mutable.WrappedArray[_]].toSet)) {
+            throw ColumnMismatch(mismatchMessage)
+          }
+          false
+        case a =>
+          true
+      }
+    }).exists(a => a)
+
+    if (isNotArrayElement && !colName1Elements.sameElements(colName2Elements)) {
       throw ColumnMismatch(mismatchMessage)
     }
+
   }
 
 }
